edf720963f414ea7aa004687fe94f0a3f37f041c
Sebastian Smela <sebastian.smela@outlook.com>
2025-04-27 18:42:28 +0200

    test

--- frontend/src/components/hoc/layout.component.tsx
+++ frontend/src/components/hoc/layout.component.tsx
@@ -1,37 +1,37 @@
-import { useEffect } from 'react';
-import { Box, Container } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import ApplicationBar from '../app-bar.component';
-import ApplicationFooter from '../app-footer.component';
+import { useEffect } from "react";
+import { Box, Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import ApplicationBar from "../app-bar.component";
+import ApplicationFooter from "../app-footer.component";
 
 const withLayout = (Component: React.ComponentType) => {
-    return function WrappedWithLayout(props: unknown) {
-        const navigate = useNavigate();
-        const token = localStorage.getItem('github_token');
+  return function WrappedWithLayout(props: unknown) {
+    const navigate = useNavigate();
+    const token = localStorage.getItem("github_token");
 
-        useEffect(() => {
-            if (!token) navigate('/');
-        }, [token, navigate]);
+    useEffect(() => {
+      if (!token) navigate("/");
+    }, [token, navigate]);
 
-        return (
-            <Box>
-                <ApplicationBar />
-                <Box
-                    sx={{
-                        background: (theme) => theme.palette.gray.raisin,
-                        minHeight: 'calc(100vh - 64px)',
-                        py: 4,
-                    }}
-                >
-                    <Container maxWidth="xl">
-                        <Component {...props} />
-                    </Container>
-                </Box>
-                <ApplicationFooter />
-            </Box>
-        );
-    };
+    return (
+      <Box>
+        <p>test</p>
+        <ApplicationBar />
+        <Box
+          sx={{
+            background: (theme) => theme.palette.gray.raisin,
+            minHeight: "calc(100vh - 64px)",
+            py: 4,
+          }}
+        >
+          <Container maxWidth="xl">
+            <Component {...props} />
+          </Container>
+        </Box>
+        <ApplicationFooter />
+      </Box>
+    );
+  };
 };
 
 export default withLayout;
-
